d086a237dfa680c6e4b0c2e27b09cb79f613d227
zakhar <zaharsavchen@gmail.com>
2024-12-28 23:27:40 +0200

    create newspaper form and newspaper search form by title

---
+++ newspapers/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from newspapers.models import Newspaper
+
+
+class NewspaperForm(forms.ModelForm):
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
+
+
+class NewspaperTitleSearchForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by title"}
+        )
+    )
