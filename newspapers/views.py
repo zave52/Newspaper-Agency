e084c7aa425bd3a35a9b2db0d87adf0f65d2b091
zakhar <zaharsavchen@gmail.com>
2024-12-28 17:32:18 +0200

    create index and lists view without get context and get queryset logic

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from newspapers.models import Newspaper, Redactor, Topic
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_visits": num_visits
+    }
+    return render(request, "newspaper_agency/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet:
+        Topic.objects.all()
+        pass
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        pass
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    paginate_by = 5
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Newspaper.objects.prefetch_related("topic")
+        pass
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        pass
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 5
