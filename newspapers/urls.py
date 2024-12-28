f063eb50ddc21f15262a754b27a49ba1a9dc5a9c
zakhar <zaharsavchen@gmail.com>
2024-12-28 21:17:49 +0200

    add paths to index and topics views

---
+++ newspapers/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from newspapers.views import index, TopicCreateView, TopicListView, \
+    TopicUpdateView, TopicDeleteView
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
+]
+
+app_name = "newspapers"
