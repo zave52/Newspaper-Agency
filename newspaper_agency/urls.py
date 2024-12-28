722bd5fbfec02d87ee25130ef0ab44fe06d34c90
zakhar <zaharsavchen@gmail.com>
2024-12-28 17:47:03 +0200

    add root path to newspapers and static file configuration in urls

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
-]
+  path("admin/", admin.site.urls),
+  path("", include("newspapers.urls", namespace="newspapers"))
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
