df59f31028b0e4b1681c632395a144eccd7cf2ee
zakhar <zaharsavchen@gmail.com>
2024-12-28 21:44:16 +0200

    add paths to redactors views

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from newspapers.views import index, TopicCreateView, TopicListView, \
     TopicUpdateView, TopicDeleteView, NewspaperListView, NewspaperCreateView, \
-    NewspaperUpdateView, NewspaperDeleteView
+    NewspaperUpdateView, NewspaperDeleteView, RedactorListView, \
+    RedactorCreateView, RedactorUpdateView, RedactorDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -14,6 +15,10 @@ urlpatterns = [
     path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
     path("newspapers/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper-update"),
     path("newspapers/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path("redactors/<int:pk>/update/", RedactorUpdateView.as_view(), name="redactor-update"),
+    path("redactors/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor-delete"),
 ]
 
 app_name = "newspapers"
